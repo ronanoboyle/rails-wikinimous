d3981c278b7c0adeca1e8de65d9061021cf4743d
Ronan OBoyle <ronanoboyle.rob@gmail.com>
2023-02-05 21:10:00 +0000

    Just New/Edit/Destroy left'

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,26 @@
 class ArticlesController < ApplicationController
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
 end
